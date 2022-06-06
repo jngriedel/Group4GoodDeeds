f46f2f7e4382e25af1513f1b7e4c6884c6d10698
Abel Tesfa <abeltesfa@gmail.com>
2022-06-06 19:05:53 -0400

    2 users in user seeder added

--- express-project-starter/db/seeders/20220606221109-userSeeder.js
+++ express-project-starter/db/seeders/20220606221109-userSeeder.js
@@ -2,25 +2,15 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+   return queryInterface.bulkInsert('Users', [
+     {firstName: 'Abe', lastName: 'Lincoln', email: 'honestabe@president.com', password: 'honestabe'},
+     {firstName: 'Rih', lastName: 'Anna', email: 'rihanna@president.com', password: 'password'}
+   ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+   return queryInterface.bulkDelete('Users', null, {});
   }
 };
