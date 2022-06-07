d4b68cdcc20a128770a7160f7a8591c2b0fb8061
Abel Tesfa <abeltesfa@gmail.com>
2022-06-07 11:55:05 -0400

    3 status seeders added

--- express-project-starter/db/seeders/20220606221601-statusSeeder.js
+++ express-project-starter/db/seeders/20220606221601-statusSeeder.js
@@ -2,25 +2,16 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+   return queryInterface.bulkInsert('Statuses', [
+    {currentStatus: 'Not Interested'},
+    {currentStatus: 'In-Progress'},
+    {currentStatus: 'Completed'},
+   ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+   return queryInterface.bulkDelete('Statuses', null, {});
   }
 };
