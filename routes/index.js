871d41d89ddd82199d964471e4f31377678af58c
abeltesfa <99838762+abeltesfa@users.noreply.github.com>
2022-06-08 16:35:07 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('******************')
-  console.log('******************')
-  console.log('******************')
-  console.log('******************')
-  console.log('******************')
-  console.log('******************')
-  console.log(res.locals.authenticaded)
+
   res.render('index', { title: 'a/A Express Skeleton Home' });
 });
 
