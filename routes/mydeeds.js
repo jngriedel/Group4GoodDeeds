3a02d558b546f4b703822c65d0df8a06607b5179
jngriedel <jngriedel@gmail.com>
2022-06-09 13:22:36 -0700

    everything but checkboxes working

--- routes/mydeeds.js
+++ routes/mydeeds.js
@@ -5,13 +5,28 @@ const db  = require('../db/models');
 const { requireAuth } = require('../auth');
 
 router.get('/', requireAuth, asyncHandler( async(req, res) => {
-
+    //get user karmas
     const karmas = await db.Karma.findAll({
         where: {
             userId: res.locals.user.id
 
         }
     })
+    //find links between karmas and ids
+    const links = [];
+    for (let i = 0; i < karmas.length; i++) {
+        const kId = karmas[i].id;
+        const link = await db.KarmasToDeed.findAll({where:{
+            karmaId: kId
+        }})
+        if (link) links.push(link)
+
+
+
+
+    }
+
+
 
     const reviews = await db.Review.findAll({
         include: [db.Deed, db.User],
@@ -30,7 +45,9 @@ router.get('/', requireAuth, asyncHandler( async(req, res) => {
       dates.push( [mnth, day, date.getFullYear()].join("-"))
 
     }
-    res.render('mydeeds', { karmas, reviews, dates });
+
+
+    res.render('mydeeds', { karmas, reviews, dates,links });
 } ))
 
 
